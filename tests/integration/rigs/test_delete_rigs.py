25f1bf4a3e276889e46155c42d0c7c17f12675c2
jessy.liao <jessy.liao@alleninstitute.org>
2025-07-30 18:02:23 -0700

    Add integration test for deleting rigs

--- tests/integration/rigs/test_delete_rigs.py
+++ tests/integration/rigs/test_delete_rigs.py
@@ -5,7 +5,26 @@
 ################################################################################
 
 
-# TODO: 
-#   - delete existing rig 
-#   - delete non-existing rig 
-#   - delete rig with calibrations <<< not sure if this works... I dont think calibrations will get cleaned up
+def test_delete_rig(client):
+    # Ensure there are 2 rigs
+    response = client.get("/api/v1beta/rigs")
+    assert response.status_code == 200
+    rigs = response.json()["output"]
+    assert len(rigs) == 2
+     
+    # Perform deletion
+    response = client.delete("/api/v1beta/rigs/foo_1_a")
+    assert response.status_code == 200
+
+    # Ensure there is a single rig left
+    response = client.get("/api/v1beta/rigs")
+    assert response.status_code == 200
+    rigs = response.json()["output"]
+    assert len(rigs) == 1
+    assert rigs[0]['rig_name'] == "bar_2_b"
+
+
+def test_delete_non_existent_rig(client):
+    response = client.delete("/api/v1beta/rigs/fake_test_rig")
+    assert response.status_code == 404
+    assert response.json()['detail'] == "Rig with name fake_test_rig not found"
