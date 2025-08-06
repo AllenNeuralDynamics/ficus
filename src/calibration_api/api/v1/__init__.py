966dc46b9fc339f17363ef5c5ba38299433ed25a
jessy.liao <jessy.liao@alleninstitute.org>
2025-08-06 13:13:04 -0700

    Add config endpoint and zk connection and crud functions

--- src/calibration_api/api/v1/__init__.py
+++ src/calibration_api/api/v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from calibration_api.api.v1.rigs import router as rigs_router
 from calibration_api.api.v1.calibrations import router as calibrations_router
+from calibration_api.api.v1.configs import router as configs_router
 
 router = APIRouter(prefix="/v1beta")
 
 router.include_router(rigs_router)
 router.include_router(calibrations_router)
+router.include_router(configs_router)
 
