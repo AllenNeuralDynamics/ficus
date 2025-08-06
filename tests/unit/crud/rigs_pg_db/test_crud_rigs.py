1ce28f4e3e242ab8e6dc549e2cf5362aa1ebd6c8
jessy.liao <jessy.liao@alleninstitute.org>
2025-08-06 14:50:23 -0700

    Move calibration/rig tests into rigs_pg_db directories

--- tests/unit/crud/rigs_pg_db/test_crud_rigs.py
+++ tests/unit/crud/rigs_pg_db/test_crud_rigs.py
@@ -1,7 +1,7 @@
 import pytest
 
 from fastapi import HTTPException
-from tests.unit.mock_database import Rigs, db_session  # noqa: F401
+from tests.unit.crud.rigs_pg_db.mock_database import Rigs, db_session  # noqa: F401
 
 from calibration_api.crud.rigs_pg_db.rigs import get_rigs, get_rig_by_name, create_rigs, update_rig, delete_rig_by_name
 from calibration_api.database.rigs_pg_db.schemas.rig import RigAddUpdate, PartialRigAddUpdate
