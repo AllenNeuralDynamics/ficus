23caab86194db100511281c085643d4639b4f4a9
jessy.liao <jessy.liao@alleninstitute.org>
2025-08-14 11:13:28 -0700

    Update zookeeper host to eng-logtool instead of localhost

--- src/calibration_api/database/zookeeper/config_server.py
+++ src/calibration_api/database/zookeeper/config_server.py
@@ -2,8 +2,8 @@ from kazoo.client import KazooClient
 
 
 def get_zk_client():
-    zk = KazooClient(hosts="127.0.0.1:2181")
-    # zk = KazooClient(hosts="eng-logtools:2181")
+    # zk = KazooClient(hosts="127.0.0.1:2181")
+    zk = KazooClient(hosts="eng-logtools:2181")
     zk.start()
     try:
         yield zk
