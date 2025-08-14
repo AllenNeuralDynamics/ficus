61d082c262e86e6b341434f67765bd3b8cc8249d
jessy.liao <jessy.liao@alleninstitute.org>
2025-08-14 12:40:34 -0700

    Handle json, yml, and misc filetypes stored on zk nodes

--- src/calibration_api/api/v1beta/configs.py
+++ src/calibration_api/api/v1beta/configs.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import PlainTextResponse
 
 from calibration_api.database.zookeeper.config_server import get_zk_client
 from kazoo.client import KazooClient
@@ -11,12 +12,9 @@ router = APIRouter(prefix="/configs", tags=["Configs"])
 
 @router.get("/projects/{project_name}") 
 def get_project_config(project_name: str, rig_name: str | None = None, zk: KazooClient = Depends(get_zk_client)):
-
     data = get_configs(zk, project_name, rig_name)
 
-    # If you want to return as plaintext yaml format, add the following: 
-    #   @router.get("/projects/{project_name}", response_class=PlainTextResponse)
-    #   yaml_output = yaml.dump(parsed_yaml, sort_keys=False)
-    #   return PlainTextResponse(yaml_output, media_type="text/yaml")
-
-    return data
+    if isinstance(data, dict):
+        return data
+    else:
+        return PlainTextResponse(data)
--- src/calibration_api/crud/zookeeper/configs.py
+++ src/calibration_api/crud/zookeeper/configs.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import yaml
 
@@ -6,24 +7,27 @@ from kazoo.client import KazooClient
 from kazoo.exceptions import NoNodeError
 
 
-def get_configs(zk: KazooClient, project_name: str, rig_name: str | None = None) -> dict:
-    
+def get_configs(zk: KazooClient, project_name: str, rig_name: str | None = None) -> dict | str:
     try:
         path = f"/projects/{project_name}/defaults/configuration" 
-        data, _ = zk.get(path)
-        content = yaml.safe_load(data.decode("utf-8"))
+        content = get_zk_node(zk, path)
     except NoNodeError:
         raise HTTPException(
             status_code=404,
             detail=f"Project with name {project_name} not found",
         )
 
+    # Deep merge with rig-specific configuration if filetype is yaml or json
     if rig_name: 
         try: 
             rig_path = f"/rigs/{rig_name}/projects/{project_name}/configuration"
-            rig_data, _ = zk.get(rig_path)
-            rig_content = yaml.safe_load(rig_data.decode("utf-8")) 
-            content = deep_merge(content, rig_content)
+            rig_content = get_zk_node(zk, rig_path)
+            if isinstance(rig_content, dict):
+                content = deep_merge(content, rig_content)
+            else: 
+                logging.debug(f"Cannot merge rig config for {rig_name} not a valid format (yaml or json) \
+                                Using rig config as is (overrides defaults)")
+                content = rig_content
         except NoNodeError: 
             logging.debug("using default, rig config not found")
             pass
@@ -48,3 +52,16 @@ def deep_merge(dict_prime, dict_mod):
             dict_prime[key] = value
     return dict_prime
 
+def get_zk_node(zk: KazooClient, path: str): 
+    data, _ = zk.get(path)
+    decoded_data = data.decode("utf-8")
+    try: 
+        content = yaml.safe_load(decoded_data)
+    except yaml.YAMLError as e: 
+        logging.error(f"Failed to decode file as YAML for node @ {path}: {e}")
+        try:
+            content = json.loads(decoded_data)
+        except json.JSONDecodeError as e: 
+            logging.error(f"Failed to decode file as JSON for node @ {path}: {e}")
+            content = decoded_data
+    return content
