2a087aca31cf84a4e72b045cacbf11b2c6431bb1
jessy.liao <jessy.liao@alleninstitute.org>
2026-04-16 08:16:49 -0700

    Remove old crud functions (no longer relevant for new endpoints)

--- src/ficus/crud/zookeeper.py
+++ src/ficus/crud/zookeeper.py
@@ -1,9 +1,8 @@
 import json
 import yaml
 
-from fastapi import HTTPException
 from kazoo.client import KazooClient
-from kazoo.exceptions import NoNodeError, NotEmptyError
+from kazoo.exceptions import NotEmptyError
 from loguru import logger
 
 
@@ -72,53 +71,3 @@ def get_node_content(zk: KazooClient, path: str) -> dict:
             logger.error(f"Failed to decode file as JSON for node @ {path}: {e}")
             content = decoded_data
     return content
-
-
-################################################################################
-#
-#   OLD CONFIGURATION FETCHING - TO BE DEPRECATED
-#
-################################################################################
-
-
-def get_configs_old(zk: KazooClient, project_name: str, rig_name: str | None = None) -> dict | str:
-    try:
-        path = f"/projects/{project_name}/defaults/configuration"
-        content = get_node_content(zk, path)
-    except NoNodeError:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Project with name {project_name} not found",
-        )
-
-    # Deep merge with rig-specific configuration if filetype is yaml or json
-    if rig_name:
-        # fix later
-        # parsed_rig_name = parse_rigs(rig_name)
-        try:
-            rig_path = f"/rigs/{rig_name}/projects/{project_name}/configuration"
-            rig_content = get_node_content(zk, rig_path)
-            if isinstance(rig_content, dict):
-                content = deep_merge(content, rig_content)
-            else:
-                logger.debug(
-                    f"Cannot merge rig config for {rig_name} not a valid format (yaml or json) \
-                                Using rig config as is (overrides defaults)"
-                )
-                content = rig_content
-        except NoNodeError:
-            logger.debug(f"using default, rig config not found for {rig_name}")
-            pass
-
-    return content
-
-
-def deep_merge(dict_prime, dict_mod):
-    for key, value in dict_mod.items():
-        if isinstance(value, dict):
-            if key not in dict_prime:
-                dict_prime[key] = type(value)()  # For subclasses of dict
-            deep_merge(dict_prime[key], dict_mod[key])
-        else:
-            dict_prime[key] = value
-    return dict_prime
